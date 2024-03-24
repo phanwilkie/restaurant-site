e51b271f726cba22a2f799268ea55c4a0480d014
Phan Wilkie <phanwilkie@gmail.come>
2024-03-24 18:55:01 +1300

    Initial commit with webpack wired up

---
+++ main.bundle.js
@@ -0,0 +1,19 @@
+(self["webpackChunkrestaurant_site"] = self["webpackChunkrestaurant_site"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log('Hello WORLD!');
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnQtc2l0ZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnSGVsbG8gV09STEQhJyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
